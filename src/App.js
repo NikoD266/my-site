906ee25de848d2ec1334ae6358f0db55e2fe28ad
Niko Detwiler <nicholasjdetwiler266@gmail.com>
2022-12-01 23:16:11 -0500

    First Commit!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import ListIcon from '@mui/icons-material/List';
+import HomeIcon from '@mui/icons-material/Home';
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div class='app'>
+        <Head />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function Head() {
+  return (
+    <div style={{ backgroundColor: "burlywood" }}
+      className="head">
+      <div className='menu'>
+        <ListIcon sx={{
+          width: 40,
+          height: 40,
+          color: 'seashell'
+        }} />
+      </div>
+      <div className='title'>
+        <h1>My Website</h1>
+      </div>
+      <div className='home'>
+        <Link to='/'>
+          <HomeIcon sx={{
+            width: 40,
+            height: 40,
+            color: 'seashell'
+          }} />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+function Home() {
+  return (
+    <div style={{ backgroundColor: "bisque" }}
+      className="body">
+      <p>This is the Body</p>
+    </div>
+  )
 }
 
 export default App;
